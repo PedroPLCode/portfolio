0e7550b92ca5b1a835c8738d293b423a0ca51548
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2025-06-24 13:44:06 +0200

    Fix js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const typingSpeed = 12;
-const nextLineDelay = 1;
-
 document.addEventListener("DOMContentLoaded", () => {
     const terminal = document.getElementById("terminal");
     const terminalContainer = document.getElementById("terminal-container");
@@ -15,14 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
             function typeLine() {
                 if (currentLine >= lines.length) return;
 
-                const line = lines[currentLine];
+                const lineObj = lines[currentLine];
+                const lineText = typeof lineObj === "string" ? lineObj : lineObj.text;
+                const typingSpeed = lineObj.typingSpeed || 25;
+                const nextLineDelay = lineObj.nextLineDelay || 10;
                 const tempDiv = document.createElement("div");
                 terminal.appendChild(tempDiv);
 
                 const interval = setInterval(() => {
-                    tempDiv.innerHTML = line.slice(0, ++currentChar);
+                    tempDiv.innerHTML = lineText.slice(0, ++currentChar);
                     terminalContainer.scrollTop = terminalContainer.scrollHeight;
-                    if (currentChar >= line.length) {
+
+                    if (currentChar >= lineText.length) {
                         clearInterval(interval);
                         currentChar = 0;
                         currentLine++;
